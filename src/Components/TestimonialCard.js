3f5524c5ab79c01b9c801a58133e0a6ba4fd873d
林庭奕 <morgan.futurenest@gmail.com>
2024-09-29 20:09:19 +0800

    home scroll plzzzzz

--- src/Components/TestimonialCard.js
+++ src/Components/TestimonialCard.js
@@ -13,6 +13,8 @@ const CardContainer = styled.div`
   border: 1.5px solid rgba(217, 217, 217, 1);
   border-radius: 12px;
   transform: rotate(${(props) => props.rotateX || '0deg'});
+  max-height: 60vh;  // 限制最大高度
+  overflow-y: auto;
 
   img {
     width: 100%; /* 圖片寬度充滿容器 */
