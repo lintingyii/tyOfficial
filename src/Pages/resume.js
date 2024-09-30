2651316116d00207f4a3dfe40f10bf2b652f127e
林庭奕 <morgan.futurenest@gmail.com>
2024-10-01 00:45:38 +0800

    resume update

--- src/Pages/resume.js
+++ src/Pages/resume.js
@@ -106,11 +106,14 @@ export const Resume = () => {
             </Column2>
           </Div4>
         </Div3>
-        <Div12>Professional Experience</Div12>
+
+        <Hr />
+
+        <Div26>Professional Experience</Div26>
         <Div13>
           <Div14>
             <Div15>
-              <Div16>UIUX Designer</Div16>
+              <Div16>Product Designer</Div16>
               <Div17> • KOL Tech CO.</Div17>
             </Div15>
             <Div18>Jan 2024 - Present / Taipei</Div18>
@@ -169,7 +172,8 @@ export const Resume = () => {
             />
           </Div23>
         </Div13>
-
+        
+        <Div34>
         <Div26>Education</Div26>
         <Div27>
           <Div28>
@@ -185,9 +189,10 @@ export const Resume = () => {
           </Div28>
           {/* <Div33>2020 - 2024</Div33> */}
         </Div27>
+        </Div34>
 
         <Div34>
-          <Div37>Skills</Div37>
+          <Div26>Skills</Div26>
           <Div35>
             <Column6>
               <Div36>
@@ -238,7 +243,7 @@ export const Resume = () => {
                     ⊛ HTML / CSS
                     <br />
                     ⊛ React.js
-                    <br />⊛ Bootstrap / Ant Design
+                    <br />⊛ Bootstrap / Ant Design / MUI
                   </Div41>
                 </Div40>
               </Div36>
@@ -246,11 +251,68 @@ export const Resume = () => {
           </Div35>
         </Div34>
 
+        <Hr />
+
+        <Div26>Project Experience</Div26>
+        <div>
+          <div
+            style={{
+              marginTop: "32px",
+              display: "flex",
+              alignItems: "center",
+              width: "100%",
+              justifyContent: "space-between",
+              gap: "8px",
+            }}
+          >
+            <Div38 style={{ margin: "0px" }}>
+              2024 總統教育獎主視覺網站設計
+            </Div38>
+            <a
+              href="https://www.figma.com/proto/cfDzQe9HdCKYSBAbAs8nH2/%E7%B8%BD%E7%B5%B1%E6%95%99%E8%82%B2%E7%8D%8E-(%E6%99%BA%E6%96%B0)?page-id=0%3A1&node-id=84-4229&node-type=canvas&viewport=32%2C-94%2C0.05&t=eS3KCLooAS6QChHs-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=84%3A4229"
+              rel="noopener noreferrer"
+              target="_blank"
+              style={{ textDecoration: "none" }}
+            >
+            <ViewMoreButton>View More</ViewMoreButton>
+            </a>
+          </div>
+          <Div31>Website Design ｜ <a href="https://www.steps.com.tw/" style={{textDecoration:'none', color:'#888'}}>智新資通</a></Div31>
+          <Div33>Aug 2024</Div33>
+        </div>
+        <div>
+          <div
+            style={{
+              marginTop: "32px",
+              display: "flex",
+              alignItems: "center",
+              width: "100%",
+              justifyContent: "space-between",
+              gap: "8px",
+            }}
+          >
+            <Div38 style={{ margin: "0px" }}>
+              政治大學國合處薦外交換生甄試系統網站設計
+            </Div38>
+            <a
+              href="https://www.figma.com/proto/RO6uRsCjdMd08G8oAjMXdO/%E6%94%BF%E5%A4%A7%E5%9C%8B%E5%90%88%E8%99%95%E8%96%A6%E5%A4%96%E4%BA%A4%E6%8F%9B%E7%94%9F%E7%94%84%E8%A9%A6%E7%B3%BB%E7%B5%B1?page-id=119%3A4450&node-id=1023-15310&node-type=canvas&viewport=305%2C173%2C0.03&t=vhjNDNzpBbQbw2J8-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=1023%3A15310"
+              rel="noopener noreferrer"
+              target="_blank"
+              style={{ textDecoration: "none" }}
+            >
+              <ViewMoreButton>View More</ViewMoreButton>
+            </a>
+          </div>
+          <Div31>Website Design ｜ <a href="https://www.steps.com.tw/" style={{textDecoration:'none', color:'#888'}}>智新資通</a></Div31>
+          <Div33>Jun 2024</Div33>
+        </div>
+
+        <Div34>
         <Div26>Honors</Div26>
         <div>
           <div
             style={{
-              marginTop: "36px",
+              marginTop: "32px",
               display: "flex",
               alignItems: "center",
               width: "100%",
@@ -275,7 +337,7 @@ export const Resume = () => {
         <div>
           <div
             style={{
-              marginTop: "36px",
+              marginTop: "32px",
               display: "flex",
               alignItems: "center",
               width: "100%",
@@ -297,6 +359,7 @@ export const Resume = () => {
           <Div31>Future young lions award, 4TH prize</Div31>
           <Div33>Mar 2023</Div33>
         </div>
+        </Div34>
       </Div2>
 
       <DownloadButton onClick={downloadPdf}>Download resume PDF<DownloadIcon/></DownloadButton>
@@ -621,13 +684,13 @@ const Img4 = styled.img`
 
 const Div12 = styled.div`
   color: #000fff;
-  margin-top: 58px;
+  margin-top: 3rem;
   font: 700 23px/150% Noto Sans TC, sans-serif;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
     Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   @media (max-width: 991px) {
     max-width: 100%;
-    margin-top: 40px;
+    margin-top: 2rem;
   }
 `;
 
@@ -735,22 +798,26 @@ const Div23 = styled.div`
 `;
 
 const Img5 = styled.img`
-  aspect-ratio: 1.06;
+  aspect-ratio: 1;
   object-fit: auto;
   object-position: center;
   width: 82px;
   margin-top: 24px;
+  border-radius: 10px;
+
   @media (max-width: 550px) {
     display: none;
   }
 `;
 
 const Img6 = styled.img`
-  aspect-ratio: 1.01;
+  aspect-ratio: 1;
   object-fit: auto;
   object-position: center;
   width: 82px;
   margin-top: 170px;
+  border-radius: 10px;
+
   @media (max-width: 550px) {
     display: none;
   }
@@ -828,14 +895,12 @@ const Img9 = styled.img`
 
 const Div26 = styled.div`
   color: #000fff;
-  margin-top: 57px;
   font: 700 23px/150% Noto Sans TC, sans-serif;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
     Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 
   @media (max-width: 991px) {
     max-width: 100%;
-    margin-top: 40px;
   }
 `;
 
@@ -873,6 +938,7 @@ const Img10 = styled.img`
   object-fit: auto;
   object-position: center;
   width: 53px;
+  border-radius: 10px;
 `;
 
 const Div29 = styled.div`
@@ -933,11 +999,11 @@ const Div33 = styled.div`
 `;
 
 const Div34 = styled.div`
-  margin-top: 56px;
+  margin-top: 7rem;
   padding: 0 1px;
   @media (max-width: 991px) {
     max-width: 100%;
-    margin-top: 40px;
+    margin-top: 5rem;
   }
 `;
 
@@ -969,10 +1035,6 @@ const Div36 = styled.div`
   font-weight: 500;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
     Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-
-  @media (max-width: 991px) {
-    margin-top: 20px;
-  }
 `;
 
 const Div37 = styled.div`
@@ -984,7 +1046,7 @@ const Div37 = styled.div`
 
 const Div38 = styled.div`
   color: #333;
-  margin-top: 36px;
+  margin-top: 32px;
   font: 500 20px/150.3% Noto Sans TC, sans-serif;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
     Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
@@ -1145,3 +1207,14 @@ const ViewMoreButton = styled.button`
     font-size: 0.8rem;
   }
 `;
+
+const Hr = () => (
+  <div style={{ display: 'flex', justifyContent: 'center', margin: '10vh 0' }}>
+    <svg width="112" height="6" viewBox="0 0 112 6" fill="none">
+      <path 
+        d="M9.24865 1.40299C11.1861 -0.467666 14.2571 -0.467663 16.1946 1.40299L17.8878 3.03785C19.0503 4.16024 20.8929 4.16024 22.0554 3.03785L23.777 1.403C25.7145 -0.467659 28.7855 -0.467659 30.723 1.403L32.4162 3.03785C33.5787 4.16025 35.4213 4.16025 36.5838 3.03785L38.277 1.403C40.2145 -0.467659 43.2855 -0.467656 45.223 1.403L46.9162 3.03786C48.0787 4.16025 49.9213 4.16025 51.0838 3.03786L52.7552 1.42402C52.7592 1.42024 52.7593 1.414 52.7555 1.41009C52.7517 1.40617 52.7519 1.39989 52.7558 1.39612C54.6936 -0.467659 57.7595 -0.465367 59.6946 1.403L61.3878 3.03785C62.5503 4.16025 64.3929 4.16025 65.5554 3.03785L67.2486 1.403C69.1861 -0.467662 72.2571 -0.467659 74.1946 1.403L75.8878 3.03785C77.0503 4.16025 78.8929 4.16025 80.0554 3.03785L81.777 1.403C83.7145 -0.467655 86.7855 -0.467655 88.723 1.403L90.4162 3.03786C91.5787 4.16025 93.4213 4.16025 94.5838 3.03786L96.277 1.403C98.2145 -0.467655 101.285 -0.467652 103.223 1.403L104.916 3.03786C106.079 4.16025 107.921 4.16025 109.084 3.03786L110.058 2.0976C110.455 1.71398 111.088 1.72509 111.472 2.1224C111.855 2.51972 111.844 3.15279 111.447 3.5364L110.473 4.47666C108.535 6.34732 105.464 6.34732 103.527 4.47666L101.834 2.84181C100.671 1.71941 98.8287 1.71941 97.6662 2.8418L95.973 4.47666C94.0355 6.34732 90.9645 6.34732 89.027 4.47666L87.3338 2.8418C86.1713 1.71941 84.3287 1.71941 83.1662 2.8418L81.4446 4.47665C79.5071 6.34731 76.4361 6.34731 74.4986 4.47665L72.8054 2.8418C71.6429 1.71941 69.8003 1.7194 68.6378 2.84179L66.9446 4.47665C65.0071 6.34731 61.9361 6.34731 59.9986 4.47665L58.3054 2.8418C57.1502 1.72643 55.3234 1.71945 54.1598 2.82085C54.1558 2.82459 54.1556 2.83085 54.1594 2.83476C54.1632 2.83865 54.1631 2.84485 54.1592 2.8486L52.473 4.47666C50.5355 6.34732 47.4645 6.34732 45.527 4.47666L43.8338 2.8418C42.6713 1.71941 40.8287 1.71941 39.6662 2.8418L37.973 4.47666C36.0355 6.34731 32.9645 6.34731 31.027 4.47666L29.3338 2.8418C28.1713 1.71941 26.3287 1.71941 25.1662 2.8418L23.4446 4.47665C21.5071 6.34731 18.4361 6.34731 16.4986 4.47665L14.8054 2.8418C13.6429 1.7194 11.8003 1.7194 10.6378 2.84179L8.94459 4.47665C7.00713 6.34731 3.93611 6.34731 1.99865 4.47665L1.02481 3.53639C0.627491 3.15278 0.616384 2.51971 0.999998 2.12239C1.38361 1.72508 2.01668 1.71397 2.41399 2.09759L3.38784 3.03785C4.55032 4.16024 6.39292 4.16024 7.5554 3.03785L9.24865 1.40299Z" 
+        fill="#D58CFE" 
+      />
+    </svg>
+  </div>
+);
