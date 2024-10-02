352e9ba02357e3bdd77ff79c66b0fe505870b91f
林庭奕 <morgan.futurenest@gmail.com>
2024-10-02 17:48:33 +0800

    resume deploy update

--- src/Pages/resume.js
+++ src/Pages/resume.js
@@ -172,23 +172,23 @@ export const Resume = () => {
             />
           </Div23>
         </Div13>
-        
+
         <Div34>
-        <Div26>Education</Div26>
-        <Div27>
-          <Div28>
-            <Img10
-              loading="lazy"
-              srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/aa40a1d537c0dedcba2ae67abcaf90d69482f6390167831da75cde4d439357ce?apiKey=fe9890881c354019b717422c47a9cee2&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa40a1d537c0dedcba2ae67abcaf90d69482f6390167831da75cde4d439357ce?apiKey=fe9890881c354019b717422c47a9cee2&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa40a1d537c0dedcba2ae67abcaf90d69482f6390167831da75cde4d439357ce?apiKey=fe9890881c354019b717422c47a9cee2&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa40a1d537c0dedcba2ae67abcaf90d69482f6390167831da75cde4d439357ce?apiKey=fe9890881c354019b717422c47a9cee2&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa40a1d537c0dedcba2ae67abcaf90d69482f6390167831da75cde4d439357ce?apiKey=fe9890881c354019b717422c47a9cee2&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa40a1d537c0dedcba2ae67abcaf90d69482f6390167831da75cde4d439357ce?apiKey=fe9890881c354019b717422c47a9cee2&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa40a1d537c0dedcba2ae67abcaf90d69482f6390167831da75cde4d439357ce?apiKey=fe9890881c354019b717422c47a9cee2&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa40a1d537c0dedcba2ae67abcaf90d69482f6390167831da75cde4d439357ce?apiKey=fe9890881c354019b717422c47a9cee2&"
-            />
-            <Div29>
-              <Div30>國立政治大學（National Chengchi University）</Div30>
-              <Div31>Bachelor of Arts - BA, Advertising</Div31>
-              <Div33>2020 - 2024</Div33>
-            </Div29>
-          </Div28>
-          {/* <Div33>2020 - 2024</Div33> */}
-        </Div27>
+          <Div26>Education</Div26>
+          <Div27>
+            <Div28>
+              <Img10
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/aa40a1d537c0dedcba2ae67abcaf90d69482f6390167831da75cde4d439357ce?apiKey=fe9890881c354019b717422c47a9cee2&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa40a1d537c0dedcba2ae67abcaf90d69482f6390167831da75cde4d439357ce?apiKey=fe9890881c354019b717422c47a9cee2&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa40a1d537c0dedcba2ae67abcaf90d69482f6390167831da75cde4d439357ce?apiKey=fe9890881c354019b717422c47a9cee2&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa40a1d537c0dedcba2ae67abcaf90d69482f6390167831da75cde4d439357ce?apiKey=fe9890881c354019b717422c47a9cee2&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa40a1d537c0dedcba2ae67abcaf90d69482f6390167831da75cde4d439357ce?apiKey=fe9890881c354019b717422c47a9cee2&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa40a1d537c0dedcba2ae67abcaf90d69482f6390167831da75cde4d439357ce?apiKey=fe9890881c354019b717422c47a9cee2&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa40a1d537c0dedcba2ae67abcaf90d69482f6390167831da75cde4d439357ce?apiKey=fe9890881c354019b717422c47a9cee2&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/aa40a1d537c0dedcba2ae67abcaf90d69482f6390167831da75cde4d439357ce?apiKey=fe9890881c354019b717422c47a9cee2&"
+              />
+              <Div29>
+                <Div30>國立政治大學（National Chengchi University）</Div30>
+                <Div31>Bachelor of Arts - BA, Advertising</Div31>
+                <Div33>2020 - 2024</Div33>
+              </Div29>
+            </Div28>
+            {/* <Div33>2020 - 2024</Div33> */}
+          </Div27>
         </Div34>
 
         <Div34>
@@ -274,10 +274,20 @@ export const Resume = () => {
               target="_blank"
               style={{ textDecoration: "none" }}
             >
-            <ViewMoreButton>View More</ViewMoreButton>
+              <ViewMoreButton>View More</ViewMoreButton>
             </a>
           </div>
-          <Div31>Website Design ｜ <a href="https://www.steps.com.tw/" target="_blank" rel="noopener noreferrer" style={{textDecoration:'none', color:'#888'}}>智新資通</a></Div31>
+          <Div31>
+            Website Design ｜{" "}
+            <a
+              href="https://www.steps.com.tw/"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ textDecoration: "none", color: "#888" }}
+            >
+              智新資通
+            </a>
+          </Div31>
           <Div33>Aug 2024</Div33>
         </div>
         <div>
@@ -303,7 +313,17 @@ export const Resume = () => {
               <ViewMoreButton>View More</ViewMoreButton>
             </a>
           </div>
-          <Div31>Website Design ｜ <a href="https://www.steps.com.tw/" target="_blank" rel="noopener noreferrer" style={{textDecoration:'none', color:'#888'}}>智新資通</a></Div31>
+          <Div31>
+            Website Design ｜{" "}
+            <a
+              href="https://www.steps.com.tw/"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ textDecoration: "none", color: "#888" }}
+            >
+              智新資通
+            </a>
+          </Div31>
           <Div33>Jun 2024</Div33>
         </div>
         <div>
@@ -317,9 +337,7 @@ export const Resume = () => {
               gap: "8px",
             }}
           >
-            <Div38 style={{ margin: "0px" }}>
-              未來巢企業形象網站
-            </Div38>
+            <Div38 style={{ margin: "0px" }}>未來巢企業形象網站</Div38>
             <a
               href="https://futurenest.ai/index.html"
               rel="noopener noreferrer"
@@ -329,66 +347,79 @@ export const Resume = () => {
               <ViewMoreButton>View More</ViewMoreButton>
             </a>
           </div>
-          <Div31>Website Design ｜ <a href="https://futurenest.ai/index.html" target="_blank" rel="noopener noreferrer" style={{textDecoration:'none', color:'#888'}}>未來巢科技股份有限公司</a></Div31>
+          <Div31>
+            Website Design ｜{" "}
+            <a
+              href="https://futurenest.ai/index.html"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ textDecoration: "none", color: "#888" }}
+            >
+              未來巢科技股份有限公司
+            </a>
+          </Div31>
           <Div33>Nov 2023</Div33>
         </div>
 
         <Div34>
-        <Div26>Honors</Div26>
-        <div>
-          <div
-            style={{
-              marginTop: "32px",
-              display: "flex",
-              alignItems: "center",
-              width: "100%",
-              justifyContent: "space-between",
-              gap: "8px",
-            }}
-          >
-            <Div38 style={{ margin: "0px" }}>
-              2024臺北市秋季程式設計節【城市通微服務大黑客松】
-            </Div38>
-            <a
-              href="/work/sports_win"
-              rel="noopener noreferrer"
-              style={{ textDecoration: "none" }}
+          <Div26>Honors</Div26>
+          <div>
+            <div
+              style={{
+                marginTop: "32px",
+                display: "flex",
+                alignItems: "center",
+                width: "100%",
+                justifyContent: "space-between",
+                gap: "8px",
+              }}
             >
-            <ViewMoreButton>View More</ViewMoreButton>
-            </a>
+              <Div38 style={{ margin: "0px" }}>
+                2024臺北市秋季程式設計節【城市通微服務大黑客松】
+              </Div38>
+              <a
+                href="/work/sports_win"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none" }}
+              >
+                <ViewMoreButton>View More</ViewMoreButton>
+              </a>
+            </div>
+            <Div31>Honorable Mention (Top Four among 111 Teams)</Div31>
+            <Div33>Sep 2024</Div33>
           </div>
-          <Div31>Honorable Mention (Top Four among 111 Teams)</Div31>
-          <Div33>Sep 2024</Div33>
-        </div>
-        <div>
-          <div
-            style={{
-              marginTop: "32px",
-              display: "flex",
-              alignItems: "center",
-              width: "100%",
-              justifyContent: "space-between",
-              gap: "8px",
-            }}
-          >
-            <Div38 style={{ margin: "0px" }}>
-              Cannes Lions Festival, 2023Taiwan Representative Competition
-            </Div38>
-            <a
-              href="/work/youngLions"
-              rel="noopener noreferrer"
-              style={{ textDecoration: "none" }}
+          <div>
+            <div
+              style={{
+                marginTop: "32px",
+                display: "flex",
+                alignItems: "center",
+                width: "100%",
+                justifyContent: "space-between",
+                gap: "8px",
+              }}
             >
-              <ViewMoreButton>View More</ViewMoreButton>
-            </a>
+              <Div38 style={{ margin: "0px" }}>
+                Cannes Lions Festival, 2023Taiwan Representative Competition
+              </Div38>
+              <a
+                href="/work/youngLions"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none" }}
+              >
+                <ViewMoreButton>View More</ViewMoreButton>
+              </a>
+            </div>
+            <Div31>Future young lions award, 4TH prize</Div31>
+            <Div33>Mar 2023</Div33>
           </div>
-          <Div31>Future young lions award, 4TH prize</Div31>
-          <Div33>Mar 2023</Div33>
-        </div>
         </Div34>
       </Div2>
 
-      <DownloadButton onClick={downloadPdf}>Download resume PDF<DownloadIcon/></DownloadButton>
+      <DownloadButton onClick={downloadPdf}>
+        Download resume PDF
+        <DownloadIcon />
+      </DownloadButton>
 
       {/* <Footer /> */}
     </Div>
@@ -449,7 +480,7 @@ const DownloadIcon = () => (
     height="20"
     fill="currentColor"
   >
-    <path d="M223.16,68.42l-16-32A8,8,0,0,0,200,32H56a8,8,0,0,0-7.16,4.42l-16,32A8.08,8.08,0,0,0,32,72V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V72A8.08,8.08,0,0,0,223.16,68.42Zm-57.5,89.24-32,32a8,8,0,0,1-11.32,0l-32-32a8,8,0,0,1,11.32-11.32L120,164.69V104a8,8,0,0,1,16,0v60.69l18.34-18.35a8,8,0,0,1,11.32,11.32ZM52.94,64l8-16H195.06l8,16Z"/>
+    <path d="M223.16,68.42l-16-32A8,8,0,0,0,200,32H56a8,8,0,0,0-7.16,4.42l-16,32A8.08,8.08,0,0,0,32,72V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V72A8.08,8.08,0,0,0,223.16,68.42Zm-57.5,89.24-32,32a8,8,0,0,1-11.32,0l-32-32a8,8,0,0,1,11.32-11.32L120,164.69V104a8,8,0,0,1,16,0v60.69l18.34-18.35a8,8,0,0,1,11.32,11.32ZM52.94,64l8-16H195.06l8,16Z" />
   </svg>
 );
 
@@ -1235,11 +1266,11 @@ const ViewMoreButton = styled.button`
 `;
 
 const Hr = () => (
-  <div style={{ display: 'flex', justifyContent: 'center', margin: '10vh 0' }}>
+  <div style={{ display: "flex", justifyContent: "center", margin: "10vh 0" }}>
     <svg width="112" height="6" viewBox="0 0 112 6" fill="none">
-      <path 
-        d="M9.24865 1.40299C11.1861 -0.467666 14.2571 -0.467663 16.1946 1.40299L17.8878 3.03785C19.0503 4.16024 20.8929 4.16024 22.0554 3.03785L23.777 1.403C25.7145 -0.467659 28.7855 -0.467659 30.723 1.403L32.4162 3.03785C33.5787 4.16025 35.4213 4.16025 36.5838 3.03785L38.277 1.403C40.2145 -0.467659 43.2855 -0.467656 45.223 1.403L46.9162 3.03786C48.0787 4.16025 49.9213 4.16025 51.0838 3.03786L52.7552 1.42402C52.7592 1.42024 52.7593 1.414 52.7555 1.41009C52.7517 1.40617 52.7519 1.39989 52.7558 1.39612C54.6936 -0.467659 57.7595 -0.465367 59.6946 1.403L61.3878 3.03785C62.5503 4.16025 64.3929 4.16025 65.5554 3.03785L67.2486 1.403C69.1861 -0.467662 72.2571 -0.467659 74.1946 1.403L75.8878 3.03785C77.0503 4.16025 78.8929 4.16025 80.0554 3.03785L81.777 1.403C83.7145 -0.467655 86.7855 -0.467655 88.723 1.403L90.4162 3.03786C91.5787 4.16025 93.4213 4.16025 94.5838 3.03786L96.277 1.403C98.2145 -0.467655 101.285 -0.467652 103.223 1.403L104.916 3.03786C106.079 4.16025 107.921 4.16025 109.084 3.03786L110.058 2.0976C110.455 1.71398 111.088 1.72509 111.472 2.1224C111.855 2.51972 111.844 3.15279 111.447 3.5364L110.473 4.47666C108.535 6.34732 105.464 6.34732 103.527 4.47666L101.834 2.84181C100.671 1.71941 98.8287 1.71941 97.6662 2.8418L95.973 4.47666C94.0355 6.34732 90.9645 6.34732 89.027 4.47666L87.3338 2.8418C86.1713 1.71941 84.3287 1.71941 83.1662 2.8418L81.4446 4.47665C79.5071 6.34731 76.4361 6.34731 74.4986 4.47665L72.8054 2.8418C71.6429 1.71941 69.8003 1.7194 68.6378 2.84179L66.9446 4.47665C65.0071 6.34731 61.9361 6.34731 59.9986 4.47665L58.3054 2.8418C57.1502 1.72643 55.3234 1.71945 54.1598 2.82085C54.1558 2.82459 54.1556 2.83085 54.1594 2.83476C54.1632 2.83865 54.1631 2.84485 54.1592 2.8486L52.473 4.47666C50.5355 6.34732 47.4645 6.34732 45.527 4.47666L43.8338 2.8418C42.6713 1.71941 40.8287 1.71941 39.6662 2.8418L37.973 4.47666C36.0355 6.34731 32.9645 6.34731 31.027 4.47666L29.3338 2.8418C28.1713 1.71941 26.3287 1.71941 25.1662 2.8418L23.4446 4.47665C21.5071 6.34731 18.4361 6.34731 16.4986 4.47665L14.8054 2.8418C13.6429 1.7194 11.8003 1.7194 10.6378 2.84179L8.94459 4.47665C7.00713 6.34731 3.93611 6.34731 1.99865 4.47665L1.02481 3.53639C0.627491 3.15278 0.616384 2.51971 0.999998 2.12239C1.38361 1.72508 2.01668 1.71397 2.41399 2.09759L3.38784 3.03785C4.55032 4.16024 6.39292 4.16024 7.5554 3.03785L9.24865 1.40299Z" 
-        fill="#D58CFE" 
+      <path
+        d="M9.24865 1.40299C11.1861 -0.467666 14.2571 -0.467663 16.1946 1.40299L17.8878 3.03785C19.0503 4.16024 20.8929 4.16024 22.0554 3.03785L23.777 1.403C25.7145 -0.467659 28.7855 -0.467659 30.723 1.403L32.4162 3.03785C33.5787 4.16025 35.4213 4.16025 36.5838 3.03785L38.277 1.403C40.2145 -0.467659 43.2855 -0.467656 45.223 1.403L46.9162 3.03786C48.0787 4.16025 49.9213 4.16025 51.0838 3.03786L52.7552 1.42402C52.7592 1.42024 52.7593 1.414 52.7555 1.41009C52.7517 1.40617 52.7519 1.39989 52.7558 1.39612C54.6936 -0.467659 57.7595 -0.465367 59.6946 1.403L61.3878 3.03785C62.5503 4.16025 64.3929 4.16025 65.5554 3.03785L67.2486 1.403C69.1861 -0.467662 72.2571 -0.467659 74.1946 1.403L75.8878 3.03785C77.0503 4.16025 78.8929 4.16025 80.0554 3.03785L81.777 1.403C83.7145 -0.467655 86.7855 -0.467655 88.723 1.403L90.4162 3.03786C91.5787 4.16025 93.4213 4.16025 94.5838 3.03786L96.277 1.403C98.2145 -0.467655 101.285 -0.467652 103.223 1.403L104.916 3.03786C106.079 4.16025 107.921 4.16025 109.084 3.03786L110.058 2.0976C110.455 1.71398 111.088 1.72509 111.472 2.1224C111.855 2.51972 111.844 3.15279 111.447 3.5364L110.473 4.47666C108.535 6.34732 105.464 6.34732 103.527 4.47666L101.834 2.84181C100.671 1.71941 98.8287 1.71941 97.6662 2.8418L95.973 4.47666C94.0355 6.34732 90.9645 6.34732 89.027 4.47666L87.3338 2.8418C86.1713 1.71941 84.3287 1.71941 83.1662 2.8418L81.4446 4.47665C79.5071 6.34731 76.4361 6.34731 74.4986 4.47665L72.8054 2.8418C71.6429 1.71941 69.8003 1.7194 68.6378 2.84179L66.9446 4.47665C65.0071 6.34731 61.9361 6.34731 59.9986 4.47665L58.3054 2.8418C57.1502 1.72643 55.3234 1.71945 54.1598 2.82085C54.1558 2.82459 54.1556 2.83085 54.1594 2.83476C54.1632 2.83865 54.1631 2.84485 54.1592 2.8486L52.473 4.47666C50.5355 6.34732 47.4645 6.34732 45.527 4.47666L43.8338 2.8418C42.6713 1.71941 40.8287 1.71941 39.6662 2.8418L37.973 4.47666C36.0355 6.34731 32.9645 6.34731 31.027 4.47666L29.3338 2.8418C28.1713 1.71941 26.3287 1.71941 25.1662 2.8418L23.4446 4.47665C21.5071 6.34731 18.4361 6.34731 16.4986 4.47665L14.8054 2.8418C13.6429 1.7194 11.8003 1.7194 10.6378 2.84179L8.94459 4.47665C7.00713 6.34731 3.93611 6.34731 1.99865 4.47665L1.02481 3.53639C0.627491 3.15278 0.616384 2.51971 0.999998 2.12239C1.38361 1.72508 2.01668 1.71397 2.41399 2.09759L3.38784 3.03785C4.55032 4.16024 6.39292 4.16024 7.5554 3.03785L9.24865 1.40299Z"
+        fill="#D58CFE"
       />
     </svg>
   </div>
